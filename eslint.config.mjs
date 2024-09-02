a9b0bfb36e3a6d0bba389eb1bd6bb13b3284a436
ilhamfused <irharijanto20@gmail.com>
2024-09-02 16:36:10 +0700

    initial

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    ignores: ["**/build/**", "**/node_modules/**", "**/public/*"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node, es2021: true },
+      parser: tsParser,
+      ecmaVersion: "latest", // Tambahkan ini
+      sourceType: "module", // Tambahkan ini
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import "dotenv/config";
+
+const app: Application = express();
+const port: number = process.env.PORT != null ? parseInt(process.env.PORT) : 3000;
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
+});
